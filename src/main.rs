77ae443f21fd55a55f5a4d40e53c2ad2cd8d96f2
Henrik <henrikfranzen3@gmail.com>
2023-07-22 18:28:34 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ use glob_walk::GlobWalkerBuilder;
 
 #[derive(Parser)]
 #[command(name = "reno")]
-#[command(author = "Henrik")]
+#[command(author = "henke443")]
 #[command(version = "0.0.1")]
 #[command(about = "A small CLI utility written in Rust that helps with searching and replacing filenames and file contents recursively using regex and glob patterns.", long_about = None)]
 #[command(next_line_help = true)]
